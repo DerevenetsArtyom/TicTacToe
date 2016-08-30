07378f2aa141166e0cef676abd51c5c2987b8bdd
DerevenetsArtyom <fenix7036@gmail.com>
2016-08-30 19:44:23 +0300

    Add urls for user_profile and login/logout

--- boardgames/urls.py
+++ boardgames/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     # Examples:
@@ -7,6 +9,10 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'main.views.home'),
+    url(r'^$', 'main.views.home', name='boardgames_home'),
 
+    # Default Login / Logout
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='boardgames_login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'boardgames_home'}, name='boardgames_logout'),
 ]
+
